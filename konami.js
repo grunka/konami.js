5847a52948511ed8dddc6b913a5cacf39cbeb0ec
John Wästerlund <john@grunka.se>
2013-02-19 04:01:56 -0800

    Create konami.js

---
+++ konami.js
@@ -0,0 +1,17 @@
+function konami(callback) {
+    var sequence = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var position = 0;
+    $(function () {
+        $(document).keyup(function (event) {
+            if (position < sequence.length && event.which == sequence[position]) {
+                position++;
+            } else {
+                position = 0;
+            }
+            if (position == sequence.length) {
+                position = 0;
+                callback();
+            }
+        });
+    });
+}

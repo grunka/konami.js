91dc5a9e28ab73d34fd682b0dc75982d80657db6
John Wästerlund <john@grunka.se>
2013-02-19 20:49:19 +0100

    Update konami.js

--- konami.js
+++ konami.js
@@ -1,17 +1,15 @@
 function konami(callback) {
     var sequence = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     var position = 0;
-    $(function () {
-        $(document).keyup(function (event) {
-            if (position < sequence.length && event.which == sequence[position]) {
-                position++;
-            } else {
-                position = 0;
-            }
-            if (position == sequence.length) {
-                position = 0;
-                callback();
-            }
-        });
+    $(document).keyup(function (event) {
+        if (position < sequence.length && event.which == sequence[position]) {
+            position++;
+        } else {
+            position = 0;
+        }
+        if (position == sequence.length) {
+            position = 0;
+            callback();
+        }
     });
 }
